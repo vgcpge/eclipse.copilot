35a3e60da2dff741538d4d3ce31be10355bd1478
vgcpge <vgcpge@gmail.com>
2023-04-28 15:12:34 +0400

    Dummy java project for manual tests

---
+++ test_java_project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vasiligulevich
+ *
+ */
+module test_java_project {
+}
---
+++ test_java_project/src/test_java_project/HelloWorld.java
@@ -0,0 +1,7 @@
+package test_java_project;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
