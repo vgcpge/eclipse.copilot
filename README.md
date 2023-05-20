704d5f5990b02e4416f396113da427a84294e42d
vgcpge <vgcpge@gmail.com>
2023-05-20 23:01:55 +0400

    Github workflows for build and release

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Tycho Build](https://github.com/vgcpge/eclipse.copilot/actions/workflows/maven.yml/badge.svg)](https://github.com/vgcpge/eclipse.copilot/actions/workflows/maven.yml)
 # Unofficial Github Copilot integration with Eclipse
 
 ![Scanner completion](images/scanner_completion.png)
@@ -11,13 +12,13 @@ Copilot is a cloud-based service provided by Github and only works with [a paid 
 
 # Prerequisites
 - [Copilot for Neovim](https://docs.github.com/en/copilot/getting-started-with-github-copilot?tool=neovim) (no need for Neovim itself)
-- Node.js [17 or *below*](https://docs.github.com/en/copilot/getting-started-with-github-copilot?tool=neovim#prerequisites-3)
+- Node.js [any version except 18](https://docs.github.com/en/copilot/getting-started-with-github-copilot?tool=neovim#prerequisites-3)
 - Java 17 or newer
 - Eclipse IDE should be configured to use Java 17 or newer
 
 # Usage
-- Download [the latest Copilot for Eclipse](https://github.com/vgcpge/eclipse.copilot/releases/latest/download/update-site.zip). It comes as an archived update site.
-- Follow [instructions](https://help.eclipse.org/latest/topic/org.eclipse.platform.doc.user/tasks/tasks-124.htm) to install the Copilot feature from the archived update site.
+- Copy the link to update site: https://vgcpge.github.io/eclipse.copilot/
+- Follow [instructions](https://help.eclipse.org/latest/topic/org.eclipse.platform.doc.user/tasks/tasks-124.htm) to install the Copilot feature from the update site.
 - A warning dialog "Trust" will request a confirmation to install unsigned content. Install on your own risk and consider gifting me a code-signing certificate.
 - When editing a text file (any source code file counts) for the first time, plug-in will request permission to access Github Copilot in your default web browser. An error will be shown if you don't have Copilot subscription.
 - A modal dialog will be shown with a code to supply in the browser. Once granted, permission is remembered by Copilot, this is a one-time operation.
---
+++ repository/releng/bump_patch_version.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+from subprocess import check_output, check_call
+from re import compile
+from sys import argv
+
+
+def current_version():
+	"""@returns: a version of Maven project in the current directory like: 0.0.10-SNAPSHOT"""
+	# Command to run
+	cmd = ['mvn', '--non-recursive', 'help:evaluate', '-Dexpression=project.version', '-q', '-DforceStdout']
+
+	# Run the command and get the output: 0.0.10-SNAPSHOT
+	output = check_output(cmd, text=True)
+
+
+	# The output may contain a trailing newline and a percent sign, so we strip those
+	return output.rstrip("\n%")
+
+def set_version(version):
+	"""
+		Sets the version of Maven project in the current directory
+	    @param version: a version like: 0.0.10-SNAPSHOT
+	"""
+	# Command to run
+	cmd = ['mvn', 'org.eclipse.tycho:tycho-versions-plugin:set-version', '-Dtycho.mode=maven', f"-DnewVersion={version}"]
+
+	# Run the command
+	check_call(cmd)
+
+def bump_patch_version(version):
+	"""
+		@param version: a version like: 0.0.10-SNAPSHOT
+		@returns: a new version with bumped patch version like: 0.0.11-SNAPSHOT
+	"""
+	# Compile a pattern to match a version string like: 0.0.10-SNAPSHOT
+	pattern = compile(r"(\d+)\.(\d+)\.(\d+)(-.*)?")
+
+	# Match the pattern against the version string
+	match = pattern.match(version)
+
+	# If the version string does not match the pattern, we raise an exception
+	if not match:
+		raise ValueError(f"Invalid version string: {version}")
+
+	# Get the major, minor and patch version numbers
+	major, minor, patch, qualifier = match.groups()
+	
+	patch = int(patch) + 1
+
+	# Return the new version string
+	return f"{major}.{minor}.{patch}{qualifier}"
+
+def bump():
+	# Get the current version
+	version = current_version()
+
+	# Print the old version
+	print("Current version", version)
+
+	# Bump the patch version
+	new_version = bump_patch_version(version)
+
+	# Set the new version
+	set_version(new_version)
+
+	# Print the new version
+	print("New version", new_version)
+
+command = {'bump': bump, 'current_version': lambda: print(current_version().replace('-SNAPSHOT', ''))}
+
+if __name__ == "__main__":
+	command[argv[1]]()
