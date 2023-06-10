b8d5f09c1ca92b7643391773ff5af61c143e404c
vgcpge <vgcpge@gmail.com>
2023-06-10 23:09:00 +0400

    Style fixes

--- org.vgcpge.copilot.ls/src/org/vgcpge/copilot/ls/SafeCloser.java
+++ org.vgcpge.copilot.ls/src/org/vgcpge/copilot/ls/SafeCloser.java
@@ -10,9 +10,11 @@ import com.google.common.base.Throwables;
 /**
  * Closes all registered Closeables strictly in reverse order of registration.
  * 
- * Differences from  {@link com.google.common.io.Closer}:
- * <li> Closing of a resource may be done out of registration thread. All registered resources need to be thread-safe.
- * <li> Close operation is final, further registrations throw  {@link ResourceClosedException and dispose given resources ASAP. 
+ * Differences from {@link com.google.common.io.Closer}:
+ * <li>Closing of a resource may be done out of registration thread. All
+ * registered resources need to be thread-safe.
+ * <li>Close operation is final, further registrations throw
+ * {@link ResourceClosedException and dispose given resources ASAP.
  * 
  * @see com.google.common.io.Closer
  */
@@ -21,21 +23,23 @@ public final class SafeCloser implements Closeable {
 	private final Deque<Closeable> stack = new ArrayDeque<>();
 	private final Object lock = new Object();
 	private boolean closed = false;
-	/** Coordinates different closing threads, preventing out-of-order disposal. **/
+	/** Coordinates different closing threads, preventing out-of-order disposal. */
 	private boolean busy = false;
 	private Throwable error = null;
 
 	/**
 	 * @param closeable the {@code Closeable} to close when {@code FinalCloser} is
 	 *                  {@linkplain #close closed}.
-	 * @throws ResourceClosedException if this {@code FinalCloser} is already closed. Given
-	 *                     {@code closeable} will be closed ASAP. The operation
-	 *                     may be done in another thread.
-	 * @throws IOException if any resources closed throw.
+	 * @throws ResourceClosedException if this {@code FinalCloser} is already
+	 *                                 closed. Given {@code closeable} will be
+	 *                                 closed ASAP. The operation may be done in
+	 *                                 another thread.
+	 * @throws IOException             if any resources closed throw.
 	 * @return the given {@code closeable}
 	 */
 	public <T extends Closeable> T register(T closeable) throws IOException {
-		if (closeable == null) // For compatibility with try-with-resources
+		// For compatibility with try-with-resources
+		if (closeable == null)
 			return closeable;
 		boolean closedCopy;
 		synchronized (lock) {
@@ -61,10 +65,10 @@ public final class SafeCloser implements Closeable {
 	}
 
 	/**
-	 * Close all registered objects. No further operations are allowed. All attempts
-	 * to register more resources will result in them being closed ASAP. May be
-	 * called multiple times.
-	 **/
+	 * Close all registered objects. No further registrations are allowed. All
+	 * attempts to register more resources will result in them being closed ASAP.
+	 * May be called multiple times.
+	 */
 	@Override
 	public void close() throws IOException {
 		synchronized (lock) {
@@ -81,7 +85,8 @@ public final class SafeCloser implements Closeable {
 					var tmp = stack.pollFirst();
 					closeable = tmp; // Prevent false leaked resource warning
 					if (closeable == null) {
-						busy = false; // Another thread may skip execution while busy == true, reset flag now
+						// Another thread may skip execution while busy == true, reset flag now
+						busy = false;
 						break;
 					}
 				}
@@ -97,8 +102,9 @@ public final class SafeCloser implements Closeable {
 			synchronized (lock) {
 				if (error != null) {
 					Throwables.propagateIfPossible(error, IOException.class);
-					throw new AssertionError(error); // not possible, because error can only be either RuntimeException
-														// or IOException
+					// not possible, because error can only be either RuntimeException, Error
+					// or IOException
+					throw new AssertionError(error);
 				}
 			}
 		}
